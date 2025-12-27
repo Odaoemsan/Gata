a53072dc19b02a6c2668b3eb6ab7784a20f5be25
عدي مغلاج <odae570211@gmail.com>
2025-12-27 23:02:59 +0000

    Try fixing this error: `Unhandled Runtime Error: FirebaseError: Firebase

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 'use client';
 // Replace with your actual Firebase project configuration
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "API_KEY",
+  authDomain: "PROJECT_ID.firebaseapp.com",
+  projectId: "PROJECT_ID",
+  storageBucket: "PROJECT_ID.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
 };
