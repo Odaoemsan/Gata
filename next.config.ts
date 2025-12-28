8ccffba0846c41e08b45747cefeadf4da9747aac
عدي مغلاج <odae570211@gmail.com>
2025-12-28 17:49:19 +0000

    هل اصبح جاهز للان

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type {NextConfig} from 'next';
 const withPWA = require('next-pwa')({
-  dest: 'public'
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
 })
 
 const nextConfig: NextConfig = {
