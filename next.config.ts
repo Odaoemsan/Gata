231264d0963bcdf4f1eda89530d7f7ee884cad93
عدي مغلاج <odae570211@gmail.com>
2025-12-29 11:54:59 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -35,9 +35,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-   experimental: {
-    serverComponentsExternalPackages: ['@firebase/auth'],
-  },
+  serverExternalPackages: ['@firebase/auth'],
 };
 
 export default withPWA(nextConfig);
